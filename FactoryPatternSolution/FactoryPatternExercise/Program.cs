69c5299970de9be2f982d98faed76088c86f5760
Joseph Poole <Joseph.poole90@gmail.com>
2022-07-20 09:21:03 -0500

    First commit

---
+++ FactoryPatternSolution/FactoryPatternExercise/Program.cs
@@ -0,0 +1,6 @@
+﻿using FactoryPatternExercise;
+
+Console.WriteLine("Please enter the number of wheels your vehicle has, 2 or 4?");
+int userInput = Convert.ToInt32(Console.ReadLine());
+var userVehicle = VehicleFactory.GetVehicle(userInput);
+userVehicle.Drive();
